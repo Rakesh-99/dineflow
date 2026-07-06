65076d67e06ff7e3183b33df88d41cd9ff1f894b
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2026-07-06 23:35:58 +0530

    ref : refactor layout component

--- client/src/Layouts/Layout.jsx
+++ client/src/Layouts/Layout.jsx
@@ -1,33 +1,41 @@
-import {Outlet} from 'react-router';
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-import {Toaster} from '../components/ui/sonner'
-import { useSelector } from 'react-redux';
-import Sidebar from '@/components/Sidebar';
-
+import { Outlet } from "react-router";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import { Toaster } from "../components/ui/sonner";
+import { useSelector } from "react-redux";
+import Sidebar from "@/components/Sidebar";
 
 const Layout = () => {
+  const { theme } = useSelector((state) => state.themeSlice);
+  const { userData } = useSelector((state) => state.currentuserSlice);
 
-    let {theme}  = useSelector(state => state.themeSlice); 
-    const {userData} = useSelector(state => state.currentuserSlice);
-    
- 
-    return (
-        <>
-        <div className={`flex duration-1000 transition-all flex-col min-h-screen ${theme === 'light' ? 'bg-[#ffff] text-gray-900 ' : 'bg-zinc-800 text-white'}`}>
-            <NavBar/>
-            <main className="flex gap-10">
-                {
-                    userData?.role === 'restaurantOwner' && 
-                    <Sidebar/>
-                }
-            <Outlet/>
-            </main>
-            <Footer/>
-            <Toaster/>
-        </div>
-        </>
-    )
-}; 
+  return (
+    <div
+      className={`flex flex-col h-screen transition-all duration-1000 ${
+        theme === "light"
+          ? "bg-white text-gray-900"
+          : "bg-zinc-800 text-white"
+      }`}
+    >
+      {/* Navbar */}
+      <NavBar />
 
-export default Layout; 
+      {/* Main Content */}
+      <main className="flex flex-1 overflow-hidden">
+        {userData?.role === "restaurantOwner" && <Sidebar />}
+
+        {/* Only this section scrolls */}
+        <section className="flex-1 overflow-y-auto">
+          <Outlet />
+        </section>
+      </main>
+
+      {/* Footer */}
+      <Footer />
+
+      <Toaster />
+    </div>
+  );
+};
+
+export default Layout;
