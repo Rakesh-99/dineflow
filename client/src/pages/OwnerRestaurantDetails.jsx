dd7d5cfe2558ebc159bc925a861eeb12852ff9a5
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2026-07-06 16:04:47 +0530

    feat : add breadcrumbs and implement  api call in client

--- client/src/pages/OwnerRestaurantDetails.jsx
+++ client/src/pages/OwnerRestaurantDetails.jsx
@@ -1,17 +1,103 @@
-import { useParams } from "react-router"
+import { Link, useLocation, useParams } from "react-router"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Button } from "@/components/ui/button"
+import { useSelector } from "react-redux"
+import { useEffect, useState } from "react"
+import axios from "axios"
+const URL = import.meta.env.VITE_BACKEND_SHOP_API_URL;
+
+
+
+
+
 
 
 const OwnerRestaurantDetails = () => {
 
 
   const {restaurantname, id} = useParams() ; 
-  console.log( restaurantname, id);
+  const {theme} = useSelector(state => state.themeSlice);
+  const location = useLocation(); 
+  const [restaurantData, setRestaurantData] = useState(null); 
+
+  
+  const data = [
+    {
+      label : 'Home',
+      path : '/'
+    }, 
+    {
+      label : 'Restaurants', 
+      path : '/restaurants'
+    }
+  ]
+  
+  console.log(restaurantData);
+  
+  useEffect(()=> { 
+  const getRestaurantInfo = async() => { 
+       try {
+      const {data} = await axios.get(`${URL}/get-owner-restaurant/${id}`, {withCredentials : true}); 
+
+      if(data.success) { 
+        setRestaurantData(data.data); 
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  getRestaurantInfo(); 
+  },[id]);
   
 
   return (
     <> 
    <div className="">
-        <h1>Owner Restaurant Details Page</h1>        
+
+            {/* breadcrumbs  */}
+            <div className="mt-5">
+              <Breadcrumb className={``}>
+                <BreadcrumbList>
+                  {
+                    data.map((links)=> {
+                      const {path, label} = links ; 
+                      return (
+                        <div className="flex items-center" key={label}>
+                          <BreadcrumbItem >
+                            <BreadcrumbLink asChild>
+                                <Link to={`${path}`}>{label}</Link>
+                            </BreadcrumbLink>
+                          </BreadcrumbItem>
+                          <BreadcrumbSeparator />
+                        </div>
+                      )
+                    })
+                  }
+                  <BreadcrumbItem>
+                    <BreadcrumbPage className={`${theme === 'dark' ? 'text-white' : 'text-zinc-700'}`}>
+                    {location.pathname.split("/")[1]}
+                    </BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+
+            {/* img banner :  */}
+            <div className="w-full">
+                  <img 
+                  src={restaurantData?.image} 
+                  alt="restaurant image banner" 
+                  className="w-full h-full object-contain"
+                  />
+            </div>
+         
     </div> 
     </>
   )
