925087c4d45b3c1626f857b9a3ebe4bf27056859
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2026-07-26 16:58:23 +0530

    feat (cloudinary fn()) : implement functionalities to delete file from cloudinary

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -3,23 +3,29 @@ import dotenv from 'dotenv' ;
 dotenv.config(); 
 import fs from 'fs'; 
 
-// cloudinary image uploader fn() : 
-const uploadOnCloudinary = async (file) => { 
 
 
-    // cloudinaruy configs : 
+
+  // cloudinaruy configs : 
     cloudinary.config({
     cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
     api_key : process.env.CLOUDINARY_API_KEY,
     api_secret : process.env.CLOUDINARY_API_SECRET
     }); 
 
+// cloudinary image uploader fn() : 
+export const uploadOnCloudinary = async (file) => { 
+
     // cloudinary api call with file : 
     try {
         const response  = await cloudinary.uploader.upload(file, {resource_type : "auto"}); 
         if(response) { 
             fs.unlinkSync(file) ; // here i am deleteing the file from the local system : 
-            return response.secure_url; 
+            return {
+                "url" : response.secure_url,
+                "public_id" : response.public_id,
+                "resource_type" : response.resource_type
+            } 
         }
     } catch (error) {
         console.log(`An unexpected error occurred while uploading file on server -> ${error}`);
@@ -28,4 +34,16 @@ const uploadOnCloudinary = async (file) => {
     }
 }; 
 
-export default uploadOnCloudinary; 
+
+export const deleteAssestFromCloudinary = async(publicIds) => { 
+    try {
+        let response ; 
+       for(let publicId of publicIds) { 
+            response = await cloudinary.uploader.destroy(publicId, {resource_type: "image"})
+       }; 
+       return true; 
+    } catch (error) {
+        console.log(`Could not delete assest from cloudinary!`, error);
+        return null; 
+    }
+}
