b61f29b78998daa80acb9ca6a2d509fcb7336130
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2026-07-29 11:53:51 +0530

    feat (menu) : add reducer fn() for state mutation

--- client/src/redux/features/currentOwnerRestaurants.slice.js
+++ client/src/redux/features/currentOwnerRestaurants.slice.js
@@ -7,7 +7,9 @@ const currentOwnerRestaurants = createSlice({
         restaurants: null
     }, 
     reducers : { 
-        setRestaurants : (state, action) => { 
+
+        // Restaurants : 
+        setCurrentRestaurantOwnerData : (state, action) => { 
             state.restaurants = action.payload; 
         },
         addNewRestaurant : (state, action) => { 
@@ -15,12 +17,20 @@ const currentOwnerRestaurants = createSlice({
         }, 
         removeRestaurant: (state, action) => {
             state.restaurants = state.restaurants.filter((restaurant)=> restaurant._id !== action.payload)
-        }
+        },
         
+        // menu Items : 
+        addMenuItemToRestaurant : (state, action) => { 
+            const {shop} = action.payload; 
+            const restaurant = state.restaurants.find(restaurant => restaurant._id === shop); 
+            if(restaurant){ 
+                restaurant.item.push(action.payload)
+            }
+        }
     }
 });
 
 
 
-export const {setRestaurants, addNewRestaurant, removeRestaurant} = currentOwnerRestaurants.actions; 
+export const {setCurrentRestaurantOwnerData, addNewRestaurant, removeRestaurant, addMenuItemToRestaurant} = currentOwnerRestaurants.actions; 
 export default currentOwnerRestaurants.reducer; 
