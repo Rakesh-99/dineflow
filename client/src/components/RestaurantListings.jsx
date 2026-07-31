ff65f3616efa74da2bdbf6a32c16cb33067c1481
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2026-07-31 13:57:59 +0530

    style  : revamp restaurant listing cards with responsive grid layout (2)

--- client/src/components/RestaurantListings.jsx
+++ client/src/components/RestaurantListings.jsx
@@ -27,34 +27,35 @@ const RestaurantListings = () => {
         <>
             {
                 restaurants ?
-                    <div className=' grid grid-cols-1 xl:grid-cols-2 gap-5 mx-5'>
+                    <div className='grid sm:grid-cols-2 grid-cols-1 mx-3 md:mx-5 lg:mx-10 xl:grid-cols-4 md:grid-cols-2 lg:grid-cols-3 gap-5 '>
                         {
                             restaurants.map((restaurant, idx) => (
                                 <div
                                     key={idx}
-                                    className={` shadow-xs p-2 border-2 rounded-md  m-auto ${theme === 'light' ? 'border-gray-100' : 'border-zinc-700'}`}>
+                                    className={`border-2 py-4 transition-all duration-200  rounded-md ${theme === 'light' ? 'border-gray-100 hover:shadow hover:shadow-customOrange/40' : 'border-zinc-700 hover:shadow-2xl hover:shadow-customOrange/20'}`}
+                                >
 
-                                    <div className="flex md:flex-row flex-col md:items-center items-start gap-2 md:gap-5 ">
+                                    <div>
 
                                         {/* image container :  */}
-                                        <div className="max-w-60 border md:max-h-52 rounded-md overflow-hidden ">
-                                            <img src={restaurant?.image.url} alt="restaurant img" className=" object-cover" />
+                                        <div className="mx-3">
+                                            <img src={restaurant?.image.url} alt="restaurant img" className="w-fit max-h-50  object-cover  transition-all rounded-md duration-300 overflow-hidden" />
                                         </div>
 
                                         {/* restaurant glance data :  */}
-                                        <div className={`flex flex-col  gap-5`}>
+                                        <div className={`w-[95%] mt-5  mx-auto`}>
                                             {/* restaurant icon and title :  */}
-                                            <div className={`flex gap-2  w-full  rounded-md border-zinc-700 items-center`}>
+                                            <div className={`flex gap-2 mb-3  w-full  rounded-md border-zinc-700 items-center`}>
                                                 <div className={`border rounded-full py-2 px-2 ${theme === 'light' ? 'bg-orange-50 border-orange-100' : 'bg-customOrange border-orange-700'}`}>
                                                     <GrRestaurant className={`${theme === 'light' ? 'text-customOrange' : 'text-orange-200'}`} />
                                                 </div>
                                                 <h2 className='font-semibold'>{restaurant?.shopName}</h2>
                                             </div>
 
-                                            <div className={`grid md:grid-cols-2 gap-4 ${theme === 'light' ? 'text-gray-500' : 'text-zinc-400'}`}>
+                                            <div className={`flex justify-around  ${theme === 'light' ? 'text-gray-500' : 'text-zinc-400'}`}>
 
                                                 {/* group 1 :  */}
-                                                <div className="text-xs flex flex-col justify-start gap-2 md:gap-4">
+                                                <div className="text-xs flex flex-col justify-start gap-5 md:gap-4">
 
                                                     <div className='flex items-center gap-1'>
                                                         <UserRound className="size-4" />
@@ -73,7 +74,7 @@ const RestaurantListings = () => {
                                                 </div>
 
                                                 {/* group 2 :  */}
-                                                <div className="text-xs flex flex-col justify-start gap-2  md:gap-4">
+                                                <div className="text-xs flex flex-col justify-start gap-5  md:gap-4">
 
                                                     <div className='flex items-center gap-1  '>
                                                         <MapPin className="size-4" />
@@ -91,17 +92,20 @@ const RestaurantListings = () => {
                                                     </div>
                                                 </div>
                                             </div>
+
+
                                         </div>
 
 
                                         {/* div of arrow btn to navigate to the restaurant details page :  */}
+
                                         <Button
 
-                                            className="flex bg-customOrange gap-2 border py-1 px-2 rounded items-center mr-10 "
+                                            className="mt-5 float-right mx-5 rounded bg-customOrange h-10 w-30"
                                             onClick={() => navigate(`/restaurantinfo/${getRestaurantName(restaurant.shopName)}/${restaurant._id}`)}
                                         >
-                                            <span className="text-xs font-bold">View</span>
-                                            <IoIosArrowDropright className="hover:cursor-pointer active:animate-ping" size={18} />
+                                            <span className="sm:text-xs text-sm! font-semibold">View</span>
+                                            <IoIosArrowDropright className="hover:cursor-pointer active:animate-ping size-5" />
 
                                         </Button>
                                     </div>
