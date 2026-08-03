a41b45fd1fc071a4d6d2beccedd301fd913f8fc7
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2026-08-03 12:18:25 +0530

    ref: separate authentication and role based route guards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,164 +1,157 @@
-import { lazy, Suspense } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Layout from "./Layouts/Layout";
-import useGetCurrentUser from "./hooks/useGetCurrentUser";
-import useGetCurrentLocation from "./hooks/useGetCurrentLocation";
-import ProtectedRoutes from "./routes/ProtectedRoutes";
-import useGetShopsOfCurrentOwner from "./hooks/useGetShopsOfCurrentOwner";
-import RestaurantOwnerRoutes from "./routes/RestaurantOwnerRoutes";
-// import { useSelector } from "react-redux";
+  import { lazy, Suspense } from "react";
+  import { createBrowserRouter, RouterProvider } from "react-router";
+  import Layout from "./Layouts/Layout";
+  import useGetCurrentUser from "./hooks/useGetCurrentUser";
+  import useGetCurrentLocation from "./hooks/useGetCurrentLocation";
+  import ProtectedRoutes from "./routes/ProtectedRoutes";
+  import useGetShopsOfCurrentOwner from "./hooks/useGetShopsOfCurrentOwner";
+  import RestaurantOwnerRoutes from "./routes/RestaurantOwnerRoutes";
+  // import { useSelector } from "react-redux";
 
-// lazy loading and code splitting :
-const Account = lazy(() => import("./pages/auth/Account"));
-const GuestRoutes = lazy(() => import("./routes/GuestRoutes"));
-const Home = lazy (()=> import("@/pages/Home")); 
+  // lazy loading and code splitting :
+  const Account = lazy(() => import("./pages/auth/Account"));
+  const GuestRoutes = lazy(() => import("./routes/GuestRoutes"));
+  const Home = lazy (()=> import("@/pages/Home")); 
 
-// Restaurant owner routes : 
-const RestaurantMenu = lazy(()=> import("@/pages/RestaurantMenu"));
-const Settings = lazy(()=> import("@/pages/Settings")); 
-const CustomerOrders = lazy(()=> import("@/pages/CustomerOrders")); 
-const Dashboard = lazy(()=> import("@/components/Dashboard"));
-const OwnerRestaurant = lazy(()=> import("@/pages/OwnerRestaurant")); 
-const CustomerOrder = lazy(()=> import("@/pages/CustomerOrder")); 
-const CustomerListings = lazy(()=> import("@/pages/CustomerLIstings")); 
-const OwnerRestaurantDetails = lazy(()=> import("@/pages/OwnerRestaurantDetails")); 
-
-
-const App = () => {
+  // Restaurant owner routes : 
+  const RestaurantMenu = lazy(()=> import("@/pages/RestaurantMenu"));
+  const Settings = lazy(()=> import("@/pages/Settings")); 
+  const CustomerOrders = lazy(()=> import("@/pages/CustomerOrders")); 
+  const Dashboard = lazy(()=> import("@/components/Dashboard"));
+  const OwnerRestaurant = lazy(()=> import("@/pages/OwnerRestaurant")); 
+  const CustomerOrder = lazy(()=> import("@/pages/CustomerOrder")); 
+  const CustomerListings = lazy(()=> import("@/pages/CustomerLIstings")); 
+  const OwnerRestaurantDetails = lazy(()=> import("@/pages/OwnerRestaurantDetails")); 
 
 
-  // some custom hooks :
-  useGetCurrentUser();
-  useGetCurrentLocation();
-  useGetShopsOfCurrentOwner();
+  const App = () => {
 
-  
-  const routers = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <Suspense fallback={<>Loading..</>}>
-          <Layout />
-        </Suspense>
-      ),
-      children: [
-        {
-          path : "/",
-          element : ( 
-            <Suspense fallback={<>Loading ..</>}>
-              <ProtectedRoutes>
-                <Home/>
-              </ProtectedRoutes>
-            </Suspense>
-          )
-        }, 
-        {
-          path : "dashboard", 
-          element : (
-            <Suspense fallback={<>Loading ..</>}>
-            <RestaurantOwnerRoutes>
-              <Dashboard/>
-            </RestaurantOwnerRoutes>
-            </Suspense>
-          )
-        }, 
-        {
-          path : 'restaurants', 
-          element : (
-            <Suspense fallback={<>Loading ..</>}>
-             <RestaurantOwnerRoutes>
-                  <OwnerRestaurant/>
-             </RestaurantOwnerRoutes>
-            </Suspense>
-          )
-        },
-        {
-          path : `restaurantinfo/:restaurantname/:id`,
-          element : ( 
-            <Suspense fallback={<>Loading ..</>}>
-              <RestaurantOwnerRoutes>
-                <OwnerRestaurantDetails/>
-              </RestaurantOwnerRoutes>
-            </Suspense>
-          )
-        }, 
-        {
-          path : 'restaurant-menu',
-          element : ( 
-            <Suspense>
-              <RestaurantOwnerRoutes>
-                <RestaurantMenu/>
-              </RestaurantOwnerRoutes>
-            </Suspense>
-          )
-        },
-        {
-          path : "settings", 
-          element : (
-            <RestaurantOwnerRoutes>
-              <Settings/>
-            </RestaurantOwnerRoutes>
-          )
-        }, 
-        {
-          path : "customer-orders", 
-          element : (
-            <Suspense fallback={<>Loading ..</>}>
-            <RestaurantOwnerRoutes>
-              <CustomerOrders/>
-            </RestaurantOwnerRoutes>
-            </Suspense>
-          )
-        }, 
-        {
-            path : "customers",
+
+    // some custom hooks :
+    useGetCurrentUser();
+    useGetCurrentLocation();
+    useGetShopsOfCurrentOwner();
+
+    
+    const routers = createBrowserRouter([
+      {
+        path: "/",
+        element: (
+          <Suspense fallback={<>Loading..</>}>
+            <Layout />
+          </Suspense>
+        ),
+        children: [
+          {
+            index : true,
+            element : ( 
+              <Suspense fallback={<>Loading ..</>}>
+                <ProtectedRoutes>
+                  <Home/>
+                </ProtectedRoutes>
+              </Suspense>
+            )
+          }, 
+          {
+            path : "dashboard", 
             element : (
-              <Suspense>
+              <Suspense fallback={<>Loading ..</>}>
+              <RestaurantOwnerRoutes>
+                <Dashboard/>
+              </RestaurantOwnerRoutes>
+              </Suspense>
+            )
+          }, 
+          {
+            path : 'restaurants', 
+            element : (
+              <Suspense fallback={<>Loading ..</>}>
+              <RestaurantOwnerRoutes>
+                    <OwnerRestaurant/>
+              </RestaurantOwnerRoutes>
+              </Suspense>
+            )
+          },
+          {
+            path : `restaurantinfo/:restaurantname/:id`,
+            element : ( 
+              <Suspense fallback={<>Loading ..</>}>
                 <RestaurantOwnerRoutes>
-                  <CustomerListings/>
+                  <OwnerRestaurantDetails/>
                 </RestaurantOwnerRoutes>
               </Suspense>
             )
-        },
-        {
-          path : "restaurant-menu", 
-          element : ( 
-            <Suspense fallback={<>Loading ..</>}>
+          }, 
+          {
+            path : 'restaurant-menu',
+            element : ( 
+              <Suspense fallback={<>Loading ..</> }>
+                <RestaurantOwnerRoutes>
+                  <RestaurantMenu/>
+                </RestaurantOwnerRoutes>
+              </Suspense>
+            )
+          },
+          {
+            path : "settings", 
+            element : (
+              <Suspense fallback={<>Loading ..</>  }>
               <RestaurantOwnerRoutes>
-                <RestaurantMenu/>
+                <Settings/>
               </RestaurantOwnerRoutes>
-            </Suspense>
-          )
-        },
-        {
-          path : "customer-order",
-          element : (
-            <Suspense>
+              </Suspense>
+          
+            )
+          }, 
+          {
+            path : "customer-orders", 
+            element : (
+              <Suspense fallback={<>Loading ..</>}>
               <RestaurantOwnerRoutes>
-                <CustomerOrder/>
+                <CustomerOrders/>
               </RestaurantOwnerRoutes>
-            </Suspense>
-          )
-        },
-        {
-          path: "account",
-          element: (
-            <Suspense fallback={<>Loading..</>}>
-              <GuestRoutes>
-                <Account />
-              </GuestRoutes>
-            </Suspense>
-          ),
-        },
-      ],
-    },
-  ]);
+              </Suspense>
+            )
+          }, 
+          {
+              path : "customers",
+              element : (
+                <Suspense fallback={<>Loading ..</>  }>
+                  <RestaurantOwnerRoutes>
+                    <CustomerListings/>
+                  </RestaurantOwnerRoutes>
+                </Suspense>
+              )
+          },
+          {
+            path : "customer-order",
+            element : (
+              <Suspense fallback ={<>Loading ..</> } >
+                <RestaurantOwnerRoutes>
+                  <CustomerOrder/>
+                </RestaurantOwnerRoutes>
+              </Suspense>
+            )
+          },
+          {
+            path: "account",
+            element: (
+              <Suspense fallback={<>Loading..</>}>
+                <GuestRoutes>
+                  <Account />
+                </GuestRoutes>
+              </Suspense>
+            ),
+          },
+        ],
+      },
+    ]);
 
-  return (
-    <>
-      <RouterProvider router={routers} />
-    </>
-  );
-};
+    return (
+      <>
+        <RouterProvider router={routers} />
+      </>
+    );
+  };
 
-export default App;
+  export default App;
--- client/src/routes/ProtectedRoutes.jsx
+++ client/src/routes/ProtectedRoutes.jsx
@@ -1,23 +1,18 @@
-import Dashboard from "@/components/Dashboard";
-import { Home } from "lucide-react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router";
 
 const ProtectedRoutes = ({ children }) => {
-  const { userData } = useSelector((state) => state.currentuserSlice);
+  const { userData } = useSelector(
+    (state) => state.currentuserSlice
+  );
 
   if (!userData) {
-    return <Navigate to={`/account`} replace />;
+    return <Navigate to="/account" replace />;
   }
 
-  switch(userData.role) { 
-    case 'restaurantOwner' : 
-      return <Dashboard/> 
-    case 'user' : 
-      return <Home/>
-    case 'deliveryAgent' : 
-      return <Dashboard/> 
-  } 
+  if(userData.role !== "user") { 
+    return <Navigate to={'/dashboard'} replace />
+  }
 
   return children;
 };
