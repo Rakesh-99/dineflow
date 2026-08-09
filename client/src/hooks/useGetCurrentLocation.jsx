68af980c8308e9ca0e936fc553374ed62042f624
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2026-08-09 23:47:57 +0530

    fix : include the userdata id , and duispatch to prevent thr unnecessary api calls

--- client/src/hooks/useGetCurrentLocation.jsx
+++ client/src/hooks/useGetCurrentLocation.jsx
@@ -11,24 +11,34 @@ import {setCurrentUserCity} from '../redux/features/currentUser.slice';
 const useGetCurrentLocation = () => {
 
     const {userData} = useSelector((state)=> state.currentuserSlice);
+    console.log(userData);
     
     const dispatch = useDispatch(); 
     useEffect(()=> { 
+
+        if(!navigator.geolocation) {
+            console.log("Geo location is not supporeted by this browser!");
+            return;
+        }
 
         navigator.geolocation.getCurrentPosition( async(position)=> { 
             const {latitude, longitude} = position.coords; 
 
             try {
-
                  const {data} = await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${location_api_key}`);
-
+                
+                 if(!data){
+                    console.log('Could not fetch the current location!');
+                    return; 
+                  }
                  const response = data?.results[0]; 
                  dispatch(setCurrentUserCity(response));
             } catch (error) {
                 console.log('Could not fetch current location ->', error);
+                return; 
             }
         })
-    },[userData]); 
+    },[userData?._id, dispatch]); 
 
 }
 
