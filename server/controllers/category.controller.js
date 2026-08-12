e8b14fd06e981777d33f6fb21d15559b3f8d7d4b
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2026-08-12 20:01:41 +0530

    feat : implement delete category

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -1,73 +1,97 @@
-import expressAsyncHandler from 'express-async-handler'; 
-import ErrorHandler from '../utils/ErrorHandler.js';
-import { uploadOnCloudinary } from '../utils/cloudinary.js';
-import userModel from '../models/user.model.js';
-import categoryModel from '../models/category.model.js';
+import expressAsyncHandler from "express-async-handler";
+import ErrorHandler from "../utils/ErrorHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import userModel from "../models/user.model.js";
+import categoryModel from "../models/category.model.js";
+import mongoose, { mongo } from "mongoose";
 
+// create category :
+export const createCategory = expressAsyncHandler(async (req, res, next) => {
+  const { categoryName } = req.body;
+  if (!categoryName) {
+    return next(new ErrorHandler(404, "Category name is required!"));
+  }
+  const file = req.file;
+  const userId = req.userId;
 
-// create category : 
-export const createCategory = expressAsyncHandler(async(req, res, next)=> { 
-    const {categoryName} = req.body; 
-    if(!categoryName) { 
-        return next(new ErrorHandler(404, "Category name is required!"))
-    }
-    const file = req.file
-    const userId = req.userId; 
+  let cloudinaryImg;
+  if (!file) {
+    return next(new ErrorHandler(404, "image is required!"));
+  }
+  cloudinaryImg = await uploadOnCloudinary(file.path);
 
+  const category = new categoryModel({
+    categoryName,
+    owner: userId,
+    image: {
+      resources_type: cloudinaryImg?.resource_type,
+      public_id: cloudinaryImg?.public_id,
+      url: cloudinaryImg?.url,
+    },
+  });
+  await category.save();
 
-    let cloudinaryImg ; 
-    if(!file) { 
-        return next(new ErrorHandler(404, 'image is required!'));
-    }
-    cloudinaryImg = await uploadOnCloudinary(file.path)
+  return res.status(201).json({
+    success: true,
+    message: `Category has been created`,
+    data: category,
+  });
+});
 
-    const user = await userModel.findOne({_id : userId}); 
- 
-    const category = new categoryModel({
-        categoryName , 
-        owner : user._id,
-        image : {
-            "resources_type" : cloudinaryImg?.resource_type,
-            "public_id" : cloudinaryImg?.public_id,
-            "url" : cloudinaryImg?.url
-        }
-    });
-    await category.save();
+// get categories of a particular restaurant owner :
+export const getCategoryForRestaurantOwner = expressAsyncHandler(async (req, res, next) => {
+  const user = req.userId;
 
-    return res.status(201).json({
-        success : true,
-        message : `Category has been created`, 
-        data : category
-    })
-}); 
+  const getAllCategory = await categoryModel.find({ owner: user }).populate("owner");
 
-// get categories of a particular restaurant owner : 
-export const getCategoryForRestaurantOwner = expressAsyncHandler(async(req, res, next)=> {
-    const user = req.userId; 
+  if (getAllCategory.length < 1) {
+    return next(new ErrorHandler(404, "No Category found !"));
+  }
 
-    const getAllCategory = await categoryModel.find({owner : user}).populate("owner"); 
+  return res.status(200).json({
+    success: true,
+    message: "Food category has been fetched",
+    data: getAllCategory,
+  });
+});
 
-    if(getAllCategory.length < 1) { 
-        return next(new ErrorHandler(404, "No Category found !"))
-    }
+// For USER -> get categories across all the restaurant owner :
+export const getCategoriesForUser = expressAsyncHandler(async (req, res, next) => {
+  const getCat = await categoryModel.find();
+  if (getCat.length < 1) {
+    return next(new ErrorHandler(404, "No categories found!"));
+  }
+  return res.status(200).json({
+    success: true,
+    message: "All categories have been fetched",
+    data: getCat,
+  });
+});
 
-    return res.status(200).json({
-        success : true, 
-        message : "Food category has been fetched",
-        data : getAllCategory
-    })
- }); 
+// Delete category :
 
- // For USER -> get categories across all the restaurant owner : 
- export const getCategoriesForUser = expressAsyncHandler(async(req, res, next)=> { 
-    const getCat = await categoryModel.find(); 
-    if(getCat.length < 1) { 
-        return next(new ErrorHandler(404, "No categories found!"))
-    }
-    return res.status(200).json({
-        success : true,
-        message : 'All categories have been fetched',
-        data : getCat
-    })
- })
+export const deleteCategory = expressAsyncHandler(async (req, res, next) => {
+  const userId = req.userId;
+  const { categoryId } = req.params;
+  console.log(categoryId);
 
+  if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+    return next(new ErrorHandler(401, "Invalid category id!"));
+  }
+
+  if (!categoryId) {
+    return next(new ErrorHandler(400, "Category Id not found!"));
+  }
+
+  const getCat = await categoryModel.findOneAndDelete({ owner: userId, _id: categoryId }, { returnDocument: "after" });
+
+  if (!getCat) {
+    return next(new ErrorHandler(404, "Category not found!"));
+  }
+
+  return res.status(201).json({
+    success: true,
+    message: "Category has been deleted",
+    data: getCat,
+  });
+});
--- server/routes/category/category.routes.js
+++ server/routes/category/category.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'; 
-import { createCategory, getCategoriesForUser, getCategoryForRestaurantOwner } from '../../controllers/category.controller.js';
+import { createCategory, deleteCategory, getCategoriesForUser, getCategoryForRestaurantOwner } from '../../controllers/category.controller.js';
 import isUserAuthenticated from '../../middlewares/auth.middleware.js';
 import isRestaurantOwner from '../../middlewares/isRestaurantOwner.js';
 import multerFileUpload from '../../middlewares/multerFileupload.middleware.js';
@@ -10,5 +10,6 @@ const categoryRoutes = express.Router();
 categoryRoutes.post('/create-category',isUserAuthenticated, isRestaurantOwner, multerFileUpload.single('image'), createCategory)
 .get('/get-categories-owner', isUserAuthenticated, isRestaurantOwner, getCategoryForRestaurantOwner)
 .get('/get-categories-user', isUserAuthenticated, getCategoriesForUser)
+.delete('/delete-category/:categoryId', isUserAuthenticated, isRestaurantOwner, deleteCategory)
 
 export default categoryRoutes; 
