1c390c349df6ab396252f989200f99949f254692
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2026-08-24 00:34:21 +0530

    style: refine page layouts and element sizing

--- client/src/components/RestaurantListings.jsx
+++ client/src/components/RestaurantListings.jsx
@@ -5,7 +5,7 @@ import { MdOutlinePhoneInTalk } from "react-icons/md";
 import { PiBuildingApartment } from "react-icons/pi";
 import { SlCalender } from "react-icons/sl";
 import { useNavigate } from "react-router";
-import { MapPin, UserRound } from "lucide-react";
+import { MapPin, MapPinned, UserRound } from "lucide-react";
 import { Button } from "./ui/button";
 import useGetShopsOfCurrentOwner from "@/hooks/useGetShopsOfCurrentOwner";
 import NoRestaurantFound from "./NoRestaurantFound";
@@ -49,31 +49,31 @@ const RestaurantListings = () => {
                         restaurants?.map((restaurant, idx) => (
                             <div
                                 key={idx}
-                                className={`border-2 py-4 transition-all duration-200  rounded-md ${theme === 'light' ? 'border-gray-100 hover:shadow hover:shadow-customOrange/40' : 'border-zinc-700 hover:shadow-2xl hover:shadow-customOrange/20'}`}
+                                className={`border-2 pb-2 transition-all duration-200  rounded-md ${theme === 'light' ? 'border-gray-100 hover:shadow hover:shadow-customOrange/40' : 'border-zinc-700 hover:shadow-2xl hover:shadow-customOrange/20'}`}
                             >
 
                                 <div>
 
                                     {/* image container :  */}
-                                    <div className="mx-3">
-                                        <img src={restaurant?.image.url} alt="restaurant img" className="w-fit max-h-50  object-cover  transition-all rounded-md duration-300 overflow-hidden" />
+                                    <div className=" relative">
+                                        <img src={restaurant?.image.url} alt="restaurant img" className="w-full  max-h-50  object-cover  transition-all rounded-tl-md rounded-tr-md duration-300 overflow-hidden" />
                                     </div>
 
                                     {/* restaurant glance data :  */}
-                                    <div className={` mt-5  mx-2 p-3 rounded-md border py-4 ${theme === 'dark' ? 'bg-mauve-800 border-mauve-700' : 'bg-mauve-50 border-mauve-50'}`}>
+                                    <div className={` mx-2  rounded-md  py-4 ${theme === 'dark' && 'bg-zinc-800'}`}>
 
                                         {/* restaurant icon and title :  */}
                                         <div className={`flex items-center gap-1`}>
-                                            <div className={`border rounded-full py-2 px-2 ${theme === 'light' ? 'bg-orange-50 border-orange-100' : 'bg-customOrange border-orange-700'}`}>
+                                            <div className={`border rounded-full py-px px-px ${theme === 'light' ? 'bg-orange-50 border-orange-100' : 'bg-customOrange border-orange-700'}`}>
                                                 <GrRestaurant className={`${theme === 'light' ? 'text-customOrange' : 'text-orange-200'}`} />
                                             </div>
-                                            <h2 className='md:text-xl text-sm  font-semibold'>{restaurant?.shopName}</h2>
+                                            <h2 className='md:text-lg text-sm  font-semibold'>{restaurant?.shopName}</h2>
                                         </div>
 
 
 
 
-                                        <div className="grid grid-cols-1 gap-2 text-[10px] md:text-xs mt-5 text-gray-400">
+                                        <div className="grid grid-cols-1 gap-2 text-[10px] md:text-xs mt-2 text-gray-400">
 
                                             <div className='flex items-center gap-1'>
                                                 <UserRound className="size-4" />
@@ -87,13 +87,13 @@ const RestaurantListings = () => {
 
 
                                             <div className='flex items-center gap-1  '>
-                                                <MapPin className="size-4" />
-                                                <span >Address1 : {restaurant?.address1}</span>
+                                                <MapPinned  className="size-4" />
+                                                <span >Address line 1 : {restaurant?.address1}</span>
                                             </div>
 
                                             <div className='flex items-center gap-1  '>
                                                 <MapPin className="size-4" />
-                                                <span >Address2 : {restaurant?.address2}</span>
+                                                <span >Address line 2 : {restaurant?.address2}</span>
                                             </div>
 
                                             <div className='flex items-center gap-1 '>
@@ -117,7 +117,7 @@ const RestaurantListings = () => {
                                     {/* div of arrow btn to navigate to the restaurant details page :  */}
 
                                     <Button
-                                        className="mt-5 float-right mx-5 rounded bg-customOrange"
+                                        className=" float-right mr-2 rounded bg-customOrange"
                                         onClick={() => navigate(`/restaurantinfo/${getRestaurantName(restaurant.shopName)}/${restaurant._id}`)}
                                     >
                                         <span className="text-xs font-semibold">View</span>
